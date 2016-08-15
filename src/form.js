aeb789d062cb79ab7422ed82e667359c310e326b
Vadim Ivanov <ivanov@morizo.ru>
2016-08-16 00:49:10 +0300

    Сделана валидация формы

--- src/form.js
+++ src/form.js
@@ -3,9 +3,21 @@
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formElem = document.querySelector('.review-form');
+  var formRatingElems = formElem.querySelectorAll('input[name="review-mark"]');
+  var formNameField = formElem.querySelector('#review-name');
+  var formReviewField = formElem.querySelector('#review-text');
+  var formSubmitBtn = formElem.querySelector('.review-submit');
+  var formNameLabel = formElem.querySelector('.review-fields-name');
+  var formReviewLabel = formElem.querySelector('.review-fields-text');
+  var formReviewFields = formElem.querySelector('.review-fields');
 
   var form = {
     onClose: null,
+
+    rating: null,
+
+    minRating: 3,
 
     /**
      * @param {Function} cb
@@ -21,6 +33,63 @@ window.form = (function() {
       if (typeof this.onClose === 'function') {
         this.onClose();
       }
+    },
+
+    getRating: function() {
+      Array.prototype.forEach.call(formRatingElems, function(star) {
+        if (star.checked === true) {
+          form.rating = star.value;
+        }
+      });
+    },
+
+    normalRating: function() {
+      return form.rating >= form.minRating;
+    },
+
+    enable: function() {
+      formSubmitBtn.disabled = false;
+    },
+
+    disable: function() {
+      formSubmitBtn.disabled = true;
+    },
+
+    validate: function() {
+      form.getRating();
+
+      if (formNameField.value !== '' && formReviewField.value !== '') {
+        formReviewFields.style.display = 'none';
+        form.enable();
+      } else {
+        formReviewFields.removeAttribute('style');
+        form.disable();
+      }
+
+      if (formNameField.value !== '') {
+        formNameLabel.style.display = 'none';
+      } else {
+        formNameLabel.removeAttribute('style');
+      }
+
+      if (form.normalRating()) {
+        formReviewLabel.style.display = 'none';
+        if (formNameField.value !== '') {
+          formReviewFields.style.display = 'none';
+          form.enable();
+        } else {
+          formReviewFields.removeAttribute('style');
+          form.disable();
+        }
+      } else {
+        formReviewLabel.removeAttribute('style');
+
+        if (formReviewField.value !== '') {
+          formReviewLabel.style.display = 'none';
+        } else {
+          formReviewLabel.removeAttribute('style');
+        }
+      }
     }
   };
 
@@ -29,6 +98,22 @@ window.form = (function() {
     evt.preventDefault();
     form.close();
   };
+
+  formNameField.oninput = function() {
+    form.validate();
+  };
+
+  formReviewField.oninput = function() {
+    form.validate();
+  };
+
+  Array.prototype.forEach.call(formRatingElems, function(star) {
+    star.onchange = function() {
+      form.validate();
+    };
+  });
+
+  form.validate();
 
   return form;
 })();
