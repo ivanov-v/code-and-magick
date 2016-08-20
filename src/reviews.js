303c701b6aaab7be34ed6f71465f4d5bc39ce77d
Vadim Ivanov <ivanov@morizo.ru>
2016-08-20 15:55:52 +0300

    Добавлена функция скрытия фильтров

--- src/reviews.js
+++ src/reviews.js
@@ -6,12 +6,6 @@ window.getData = function(data) {
   reviews = data;
 };
 
-var parseData = function(loadedData) {
-  loadedData.forEach(function(item) {
-    console.log(item.author.name);
-  });
-};
-
 var addScript = function(url, callback) {
   var script = document.createElement('script');
   script.src = url;
@@ -19,6 +13,18 @@ var addScript = function(url, callback) {
   document.body.appendChild(script);
 };
 
+var parseData = function(loadedData) {
+  loadedData.forEach(function(item) {
+    console.log(item.author.name);
+  });
+};
+
+var hideFilters = function() {
+  document.querySelector('.reviews-filter').classList.add('invisible');
+};
+
 addScript('http://localhost:1506/api/reviews?callback=getData', function() {
   parseData(reviews);
 });
+
+hideFilters();
