6c251af6becd5b9160be43dd9ca5b76b5b47ea7a
Vadim Ivanov <ivanov@morizo.ru>
2016-08-20 17:18:06 +0300

    Добавлена функция обработки отзывов

--- src/reviews.js
+++ src/reviews.js
@@ -1,7 +1,17 @@
 'use strict';
 
-var reviews;
+var filters = document.querySelector('.reviews-filter');
+var reviewsContainer = document.querySelector('.reviews-list');
+var templateElement = document.querySelector('#review-template');
+var elementToClone;
 
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.review');
+} else {
+  elementToClone = templateElement.querySelector('.review');
+}
+
+var reviews;
 window.getData = function(data) {
   reviews = data;
 };
@@ -13,18 +23,53 @@ var addScript = function(url, callback) {
   document.body.appendChild(script);
 };
 
-var parseData = function(loadedData) {
-  loadedData.forEach(function(item) {
-    console.log(item.author.name);
-  });
-};
-
 var hideFilters = function() {
-  document.querySelector('.reviews-filter').classList.add('invisible');
+  filters.classList.add('invisible');
 };
 
-addScript('http://localhost:1506/api/reviews?callback=getData', function() {
-  parseData(reviews);
-});
+var showFilters = function() {
+  filters.classList.remove('invisible');
+};
 
-hideFilters();
+var IMAGE_LOAD_TIMEOUT = 10000;
+
+var getReviewElement = function(review, container) {
+  var element = elementToClone.cloneNode(true);
+  element.querySelector('.review-text').textContent = review.description;
+  element.querySelector('.review-rating').textContent = review.rating;
+  container.appendChild(element);
+
+  var image = new Image();
+  var imageLoadTimeout;
+  image.onload = function(evt) {
+    clearTimeout(imageLoadTimeout);
+    var reviewImageElement = element.querySelector('.review-author');
+    reviewImageElement.src = evt.target.src;
+    reviewImageElement.title = review.author.name;
+    reviewImageElement.width = 124;
+    reviewImageElement.height = 124;
+  };
+  image.onerror = function() {
+    element.classList.add('review-load-failure');
+  };
+  image.src = review.author.picture;
+  imageLoadTimeout = setTimeout(function() {
+    image.src = '';
+    element.classList.add('review-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+  return element;
+};
+
+var initReviews = function() {
+  hideFilters();
+
+  addScript('http://localhost:1506/api/reviews?callback=getData', function() {
+    reviews.forEach(function(review) {
+      getReviewElement(review, reviewsContainer);
+    });
+  });
+
+  showFilters();
+};
+
+initReviews();
