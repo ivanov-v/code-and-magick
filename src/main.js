842123cdefd9ac8a57d3100f9340064c6072d201
Vadim Ivanov <ivanov@morizo.ru>
2016-08-22 23:16:04 +0300

    В старых файлах сделаны экспорты

--- src/main.js
+++ src/main.js
@@ -1,27 +1,25 @@
 'use strict';
 
-require('./form');
-require('./game');
+var Game = require('./game');
+var form = require('./form');
 require('./reviews');
 
-(function() {
-  var game = new window.Game(document.querySelector('.demo'));
-  game.initializeLevelAndStart();
-  game.setGameStatus(window.Game.Verdict.INTRO);
+var game = new Game(document.querySelector('.demo'));
+game.initializeLevelAndStart();
+game.setGameStatus(Game.Verdict.INTRO);
 
-  var formOpenButton = document.querySelector('.reviews-controls-new');
+var formOpenButton = document.querySelector('.reviews-controls-new');
 
-  /** @param {MouseEvent} evt */
-  formOpenButton.onclick = function(evt) {
-    evt.preventDefault();
+/** @param {MouseEvent} evt */
+formOpenButton.onclick = function(evt) {
+  evt.preventDefault();
 
-    window.form.open(function() {
-      game.setGameStatus(window.Game.Verdict.PAUSE);
-      game.setDeactivated(true);
-    });
-  };
+  form.open(function() {
+    game.setGameStatus(Game.Verdict.PAUSE);
+    game.setDeactivated(true);
+  });
+};
 
-  window.form.onClose = function() {
-    game.setDeactivated(false);
-  };
-})();
+form.onClose = function() {
+  game.setDeactivated(false);
+};
