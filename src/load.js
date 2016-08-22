0d72c33af70bd2020ac955937339deddff67eb64
Vadim Ivanov <ivanov@morizo.ru>
2016-08-22 23:02:26 +0300

    Функции разнесены по модулям

--- src/load.js
+++ src/load.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function() {
-
+window.getData = function(data) {
+  window.reviews = data;
 };
+
+var load = function(url, callback) {
+  var script = document.createElement('script');
+  script.src = url;
+  script.onload = callback;
+  document.body.appendChild(script);
+};
+
+module.exports = load;
