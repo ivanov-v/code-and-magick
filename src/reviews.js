4bab1c0e2cceef2264bed0dc7e45c051267b3a6c
Vadim Ivanov <ivanov@morizo.ru>
2016-08-29 21:52:37 +0300

    Исправил показ списка отзывов

--- src/reviews.js
+++ src/reviews.js
@@ -2,24 +2,23 @@
 
 var load = require('./load');
 var getReviewElement = require('./review');
-
 var filters = document.querySelector('.reviews-filter');
 var reviewsContainer = document.querySelector('.reviews-list');
+
+var hideFilters = function() {
+  filters.classList.add('invisible');
+};
+
+var showFilters = function() {
+  filters.classList.remove('invisible');
+};
 
 hideFilters();
 
 load('http://localhost:1506/api/reviews?callback=getData', function() {
   window.reviews.forEach(function(review) {
+    console.log(review);
     getReviewElement(review, reviewsContainer);
+    showFilters();
   });
 });
-
-showFilters();
-
-function showFilters() {
-  filters.classList.add('invisible');
-}
-
-function hideFilters() {
-  filters.classList.remove('invisible');
-}
